cb6360ccc0955ebdc4eee4c9d6baf3064be89fd6
Reddy Nithin Reddy <129539615+Nithinreddy000@users.noreply.github.com>
2024-05-29 07:57:51 +0530

    Add files via upload

---
+++ src/components/BlankPage/BlankPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlankPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Blank Page</h1>
+        </div>
+    );
+};
+
+export default BlankPage;
